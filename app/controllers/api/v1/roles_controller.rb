4de22b8e9cc6269ba7510a1424803afcfb6b10b5
lucas-perata <contacto.perata@gmail.com>
2022-07-13 10:51:36 -0300

    OT260-16: modelo, migración y controlador base de Roles

---
+++ app/controllers/api/v1/roles_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class RolesController < ApplicationController
+    end
+  end
+end
---
+++ spec/factories/roles.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :role do
+    name { 'MyString' }
+    description { 'MyString' }
+  end
+end
