f1bdd2b17ddce99fe74bc620d3d3bda93ab719a7
rquinterosv <rafael.quinterosv@gmail.com>
2022-07-13 17:50:40 -0400

    OT260-18: Crear modelo, migración y controlador base de Categories

---
+++ app/models/category.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id           :bigint           not null, primary key
+#  description  :string
+#  discarded_at :datetime
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_discarded_at  (discarded_at)
+#
+class Category < ApplicationRecord
+  include Discard::Model
+
+  has_one_attached :image
+  validates :name, presence: true
+end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id           :bigint           not null, primary key
+#  description  :string
+#  discarded_at :datetime
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_discarded_at  (discarded_at)
+#
+FactoryBot.define do
+  factory :category do
+    name { 'MyString' }
+    description { 'MyString' }
+  end
+end
