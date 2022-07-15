a38c3aeaf1eb12d9ddb36734b35670dbbec7d024
Jescobarmora <jescobarmora.cb@hotmail.com>
2022-07-15 16:55:32 -0500

    OT260-43 Categories' data update Endpoint

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,6 +3,26 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
+      before_action :admin?, only: %i[update]
+
+      # PUT /categories/:id
+
+      def update
+        if @category.update(category_params)
+          render json: CategorySerializer.new(@category).serializable_hash.to_json
+        else
+          render json: { errors: @category }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_category
+        @category = Category.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Could not find category with ID '#{params[:id]}'" },
+               status: :unprocessable_entity
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'organization/:id/public', to: 'organizations#public'
       resources :organizations, only: [:create, :update] 
+      resources :categories, only: [:update]
     end
   end
 end
