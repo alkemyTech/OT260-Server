afe64743f51e7d5a8b218eeb3aead5a3f0453769
rquinterosv <rafael.quinterosv@gmail.com>
2022-07-15 14:51:42 -0400

    feat/OT260-41-Endpoint-detalle-categoria

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,6 +3,13 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
+      before_action :authorization
+
+      # GET /category_id/public
+
+      def public
+        @category = Category.find(params[:id])
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      get 'id/public', to: 'categories#public'
     end
   end
 end
