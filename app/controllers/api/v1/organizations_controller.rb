b3a77cdd3dbd9cbd921b3a87f0a88e7fbbb763fd
Jescobarmora <jescobarmora.cb@hotmail.com>
2022-07-15 14:36:54 -0500

    OT260-37 Organization's public data update and create Endpoint

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -4,12 +4,49 @@ module Api
   module V1
     class OrganizationsController < ApplicationController
       before_action :authorization
+      before_action :set_organization, only: %i[update]
+      before_action :admin?, only: %i[update]
 
-      # GET /organization_id/public
+      # GET /organization/:id/public
 
       def public
         @organization = Organization.find(params[:id])
         render json: OrganizationSerializer.new(@organization).serializable_hash.to_json
+      end
+
+      # POST /organization/
+
+      def create
+        @organization = Organization.new(organization_params)
+        if @organization.save
+          render json: OrganizationSerializer.new(@organization).serializable_hash.to_json,
+                 status: :created
+        else
+          render json: { errors: @organization }, status: :unprocessable_entity
+        end
+      end
+
+      # PUT /organization/:id/public
+
+      def update
+        if @organization.update(params.require(:name, :image, :phone, :address))
+          render json: OrganizationSerializer.new(@organization).serializable_hash.to_json
+        else
+          render json: { errors: @organization }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def organization_params
+        params.permit(:about_us_text, :address, :email, :name, :phone, :welcome_text, :image)
+      end
+
+      def set_organization
+        @organization = Organization.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Could not find organization with ID '#{params[:id]}'" },
+               status: :unprocessable_entity
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get ':id/public', to: 'organizations#public'
+      get 'organization/:id/public', to: 'organizations#public'
+      resources :organizations, only: [:create, :update] 
     end
   end
 end
