8c0848a30697bf708701507f14c4f913444614af
Juan Andres Escobar Mora <92605158+Jescobarmora@users.noreply.github.com>
2022-07-15 16:44:39 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get ':id/public', to: 'organizations#public'
+      resources :categories, only: [:update]
     end
   end
 end
