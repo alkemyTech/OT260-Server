1a1a3e76ef835ababb84e89b975c5be9b29e85ff
Jescobarmora <jescobarmora.cb@hotmail.com>
2022-07-16 17:27:01 -0500

    OT260-45 News' show endpoint

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -3,14 +3,19 @@
 module Api
   module V1
     class NewsController < ApplicationController
-      before_action :authenticate_request, only: %i[create]
-      before_action :authorize_user, only: %i[create]
+      before_action :set_news, only: %i[show]
+      before_action :authenticate_request, only: %i[show create]
+      before_action :authorize_user, only: %i[show create]
+
+      def show
+        render json: NewsSerializer.new(@news).serializable_hash
+      end
 
       def create
         @news = News.new(news_params)
         @news.category = Category.find(params[:news][:category_id])
         if @news.save
-          render json: NewsSerializer.new(@news).serializable_hash.to_json,
+          render json: NewsSerializer.new(@news).serializable_hash,
                  status: :created
         else
           render json: { errors: @news }, status: :unprocessable_entity
@@ -18,6 +23,12 @@ module Api
       end
 
       private
+
+      def set_news
+        @news = News.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Could not find news with ID '#{params[:id]}'" }
+      end
 
       def news_params
         params.require(:news).permit(:content, :image, :name, :news_type)
