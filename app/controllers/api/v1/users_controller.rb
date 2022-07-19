185aa09e7860c4519a5e25f2f5a11e85dfbf6668
lucas-perata <contacto.perata@gmail.com>
2022-07-19 11:08:46 -0300

    feat/OT260-24 LOGIN

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,27 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def login
-        @user = User.find_by(email: params[:user][:email])
+      before_action :fetch_user_by_email, only: [:login]
 
-        if @user&.authenticate(params[:password])
-          render json: @user
+      def fetch_user_by_email
+        @user = User.find_by(email: params[:user][:email])
+      end
+
+      def login
+        if @user&.authenticate(params[:user][:password])
+          token = JsonWebToken.encode(user_id: @user.id)
+          time = Time.zone.now + 24.hours.to_i
+          render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                         username: @user.first_name }, status: :ok
         else
           render json: { ok: false }
         end
+      end
+
+      private
+
+      def login_params
+        params.permit(:email, :password)
       end
     end
   end
