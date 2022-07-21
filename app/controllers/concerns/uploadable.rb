3b8f20c15986821549c617dffcf590128c0c0eb6
gaston-amaya <amayagaston.sk@gmail.com>
2022-07-21 19:22:19 -0300

    changed some words to snake_case

--- app/controllers/concerns/uploadable.rb
+++ app/controllers/concerns/uploadable.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD:app/controllers/concerns/uploadable.rb
 module Uploadable
-  def upload
-    name = 'cat5.jpg'
-=======
-module UploadService
-  def upload(filepath)
-    key = File.basename(filepath)
->>>>>>> changed path so that its written in the method instead of writing it manually:app/controllers/concerns/upload_service.rb
+  def upload(file_path)
+    key = File.basename(file_path)
     s3 = Aws::S3::Resource.new(region: 'us-east-1')
     obj = s3.bucket(ENV.fetch('AWS_BUCKET')).object(key)
-    obj.upload_file(filepath)
+    obj.upload_file(file_path)
     obj.public_url
   end
 end
