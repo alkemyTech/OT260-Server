6bca58d3a410ee272d0e3841f1a9d5b9ca0448f0
gaston-amaya <85085480+gaston-amaya@users.noreply.github.com>
2022-07-22 16:47:01 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post "auth/login", to: "authentication#login"
+      post "auth/register", to: "users#create"
     end
   end
 end
