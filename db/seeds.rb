81f7ed6d91b5e2b7ffae1164791b5ca119a2199e
lucas-perata <contacto.perata@gmail.com>
2022-07-22 10:16:50 -0300

    feat/OT260

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    require 'faker'
+
+    Role.create(name: "administrator" , description: "admin")
+    Role.create(name: "standard" , description: "standard user")
+    Role.create(name: "regular" , description: "regular user") 
+
+    User.create(first_name: "admin", last_name: "admin", email: "admin@mail.com", password_digest: "password-admin", role_id: 1)
+
+    (1..10).each do 
+        User.create!(
+            first_name: Faker::Name.first_name, 
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email,
+            password_digest: "password-standard", 
+            role_id: 2
+        )
+    end 
+
+    (1..10).each do 
+        User.create!(
+            first_name: Faker::Name.first_name, 
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email,
+            password_digest: "password-regular", 
+            role_id: 3
+        )
+    end 
