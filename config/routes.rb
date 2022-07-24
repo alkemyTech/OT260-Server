c1613a25932019f049480941123ee6841c0c96d6
Fernando Ciballero <fernandociballero@gmail.com>
2022-07-24 12:23:51 -0300

    fix: authentication logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      post "auth/login", to: "authentication#login"
-      post "auth/register", to: "users#create"
+      post 'auth/login', to: 'users#login'
+      post 'auth/register', to: 'users#create'
     end
   end
 end
