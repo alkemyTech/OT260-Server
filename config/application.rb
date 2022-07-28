5c4a5c014d64b7374ec567d2f5aa6c8e0f4155ce
Gabriel <gabs1995@hotmail.com>
2022-07-28 15:50:48 -0500

    OT260-53: Add seeds for activities

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module OT260Server
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Set spanish as a default language
+    config.i18n.default_locale = :es
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
