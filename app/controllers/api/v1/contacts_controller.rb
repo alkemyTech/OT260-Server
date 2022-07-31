bc0c36769ae7aa9c85693e7504b871637733947e
Fernando Ciballero <fernandociballero@gmail.com>
2022-07-31 01:22:37 -0300

    OT260-62: Endpoint listar contactos

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -3,6 +3,12 @@
 module Api
   module V1
     class ContactsController < ApplicationController
+      before_action :authenticate_request, only: %i[index]
+
+      def index
+        @contact = @current_user.contacts.all
+        render json: ContactSerializer.new(@contact).serializable_hash
+      end
     end
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,10 +12,16 @@
 #  phone        :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  user_id      :bigint           not null
 #
 # Indexes
 #
 #  index_contacts_on_discarded_at  (discarded_at)
+#  index_contacts_on_user_id       (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 require 'rails_helper'
 
