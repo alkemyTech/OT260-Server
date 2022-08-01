06b2d2a77b6e2fee1dd68b5f7ffeed4efe5233af
Jescobarmora <jescobarmora.cb@hotmail.com>
2022-08-01 09:29:33 -0500

    feat/OT-260-90-add-members-documentation

--- spec/requests/api/v1/members_spec.rb
+++ spec/requests/api/v1/members_spec.rb
@@ -3,7 +3,118 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Members', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  get 'Get Members Pagination' do
+    path '/api/v1/members' do
+      response '200', 'ok' do
+        tags 'Members'
+        consumes 'application/json'
+        security [Bearer: {}]
+
+        parameter name: 'Authorization', in: :header, type: :string
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+
+  post 'Create Member' do
+    path '/api/v1/members' do
+      response '201', 'member created' do
+        tags 'Members'
+        consumes 'application/json'
+        security [Bearer: {}]
+
+        parameter name: :member, in: :body, schema: {
+          type: :object,
+          properties: {
+            description: { type: :string },
+            facebook_url: { type: :string },
+            instagram_url: { type: :string },
+            linkedin_url: { type: :string },
+            name: { type: :string },
+            image: { type: :string }
+          },
+          required: %w[name]
+        }
+        parameter name: 'Authorization', in: :header, type: :string
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+
+  put 'Update Member' do
+    path '/api/v1/members/{id}' do
+      response '200', 'member updated' do
+        tags 'Members'
+        consumes 'application/json'
+        security [Bearer: {}]
+
+        parameter name: :member, in: :body, schema: {
+          type: :object,
+          properties: {
+            description: { type: :string },
+            facebook_url: { type: :string },
+            instagram_url: { type: :string },
+            linkedin_url: { type: :string },
+            name: { type: :string },
+            image: { type: :string }
+          },
+          required: %w[name]
+        }
+
+        parameter name: 'Authorization', in: :header, type: :string
+        let(:id) { member.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
+  end
+
+  delete 'Delete Member' do
+    path '/api/v1/members/{id}' do
+      response '200', 'member deleted' do
+        tags 'Members'
+        consumes 'application/json'
+        security [Bearer: {}]
+
+        parameter name: 'Authorization', in: :header, type: :string
+        let(:id) { member.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
   end
 end
